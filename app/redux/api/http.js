55be3a961b40dc03592ea68af28c7171e225514c
SUHONG\topic <topic828@Naver.com>
2023-02-17 03:35:37 +0900

    :recycle: baseurl내 서버 url을  env의 API_URL로 변경
    env 에 API_URL 입력,  http url 변경

--- app/redux/api/http.js
+++ app/redux/api/http.js
@@ -1,23 +1,19 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {API_URL} from '@env';
 
 const http = axios.create({
-  baseURL: 'http://f1rstweb.shop',
+  baseURL: `${API_URL}`,
   timeout: 100000,
   headers: {
     'content-type': 'application/json;charset=UTF-8',
     accept: 'application/json,',
   },
 });
-// let storedToken;
-// async function fetchToken() {
-//   const storedToken = await AsyncStorage.getItem('authorization');
-//   return storedToken;
-// }
 
 http.interceptors.request.use(async function (config) {
   const storedToken = await AsyncStorage.getItem('authorization');
-  // console.log(storedToken);
+
   if (storedToken !== null || storedToken !== undefined) {
     // console.log(access_token);
     // config.headers.common["Authorization"] = `${access_token}`;
@@ -27,54 +23,3 @@ http.interceptors.request.use(async function (config) {
 });
 
 export default http;
-
-// // const fetchToken = async () => {
-// //   const token = await AsyncStorage.getItem('authorization');
-// //   return token;
-// // };
-
-// http.interceptors.request.use(async function (config) {
-//   await fetchToken();
-//   console.log(token);
-//   console.log('들어오나');
-//   console.log(storedToken);
-
-//   if (storedToken !== null || storedToken !== undefined) {
-//     // config.headers.common["Authorization"] = `${access_token}`;
-//     config.headers['Authorization'] = `${storedToken}`;
-//   }
-//   return config;
-// });
-
-// export default http;
-
-// import axios from 'axios';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import {useSelector} from 'react-redux';
-// let access_token;
-
-// const http = axios.create({
-//   baseURL: '서버주소',
-//   timeout: 100000,
-//   headers: {
-//     'content-type': 'application/json;charset=UTF-8',
-//     accept: 'application/json,',
-//   },
-// });
-// let storedToken;
-// async function fetchToken() {
-//   storedToken = await AsyncStorage.getItem('authorization');
-//   console.log('stored토큰', storedToken);
-//   return storedToken;
-// }
-
-// http.interceptors.request.use(async function (config) {
-//   await fetchToken()
-//   if (storedToken !== null || storedToken !== undefined) {
-//     config.headers['Authorization'] = `${storedToken}`;
-//   }
-
-//   return config;
-// });
-
-// export default http;
