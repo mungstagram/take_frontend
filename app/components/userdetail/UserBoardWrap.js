9f7cc07240ac17df70ce9806388f9a1e8e32e77e
SUHONG\topic <topic828@Naver.com>
2023-02-19 02:04:51 +0900

    :poop: UserBoardWrap 컴퍼넌트의 relative이 잘 안됨 수정 예정
    UserBoardWrap 컴퍼넌트의 relative이 잘 안됨 수정 예정

--- app/components/userdetail/UserBoardWrap.js
+++ app/components/userdetail/UserBoardWrap.js
@@ -1,4 +1,4 @@
-import {reverse} from 'dns';
+import {relative} from 'path';
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Colors} from '../../constants/colors';
@@ -6,15 +6,8 @@ import {Colors} from '../../constants/colors';
 const UserBoardWrap = () => {
   return (
     <View style={styles.Wrapper}>
-      <View style={styles.Container2}></View>
-      <View style={styles.Container1}>
-        <View style={styles.TabContainer}>
-          <Text> 가나다</Text>
-        </View>
-        <View style={styles.TabContainerBehind}>
-          <Text> 가나다</Text>
-        </View>
-      </View>
+      <View style={styles.TabWrapper}></View>
+      <View style={styles.ContentWrapper}></View>
     </View>
   );
 };
@@ -25,51 +18,94 @@ const styles = StyleSheet.create({
   Wrapper: {
     flex: 1,
     backgroundColor: 'green', // 여백있는지 표시
-    // marginTop: 10,
-    flexDirection: 'column-reverse',
   },
-  Wrapper2: {
-    flex: 1,
-    backgroundColor: 'blue',
-  },
-  TextStyle: {
-    fontSize: 16,
-  },
-  Container1: {
-    flex: 0.1,
-    // : ,
-    // width: 20,
+  TabWrapper: {
+    height: '15%',
+    position: 'relative',
+    top: 0,
     backgroundColor: 'red',
-    flexDirection: 'row',
-    // alignItems: 'center',
-    alignItems: 'stretch',
   },
-  Container2: {
-    flex: 1.2,
-    backgroundColor: 'white',
-    elevation: 8,
-  },
-  TabContainer: {
-    alignItems: 'center',
-    flex: 1,
-    // borderRadius: 20,
-    backgroundColor: 'white',
-    // borderTopEndRadius: 60,
-    // borderTopStartRadius: 60,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    elevation: 8,
-    borderBottomWidth: 0,
-  },
-  TabContainerBehind: {
-    alignItems: 'center',
-    flex: 1,
-    // borderRadius: 20,
-    backgroundColor: 'white',
-    // borderTopEndRadius: 60,
-    // borderTopStartRadius: 60,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    elevation: 2,
+  ContentWrapper: {
+    // width: '66%',
+    height: '70%',
+    position: 'relative',
+    bottom: 0,
+    backgroundColor: 'gray',
+    // zIndex: 4,
   },
 });
+
+// import React from 'react';
+// import {View, Text, StyleSheet} from 'react-native';
+// import {Colors} from '../../constants/colors';
+
+// const UserBoardWrap = () => {
+//   return (
+//     <View style={styles.Wrapper}>
+//       <View style={styles.Container2}></View>
+//       <View style={styles.Container1}>
+//         <View style={styles.TabContainer}>
+//           <Text> 가나다</Text>
+//         </View>
+//         <View style={styles.TabContainerBehind}>
+//           <Text> 가나다</Text>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default UserBoardWrap;
+
+// const styles = StyleSheet.create({
+//   Wrapper: {
+//     flex: 1,
+//     backgroundColor: 'green', // 여백있는지 표시
+//     // marginTop: 10,
+//     flexDirection: 'column-reverse',
+//   },
+//   Wrapper2: {
+//     flex: 1,
+//     backgroundColor: 'blue',
+//   },
+//   TextStyle: {
+//     fontSize: 16,
+//   },
+//   Container1: {
+//     flex: 0.1,
+//     // : ,
+//     // width: 20,
+//     backgroundColor: 'red',
+//     flexDirection: 'row',
+//     // alignItems: 'center',
+//     alignItems: 'stretch',
+//   },
+//   Container2: {
+//     flex: 1.2,
+//     backgroundColor: 'white',
+//     elevation: 8,
+//   },
+//   TabContainer: {
+//     alignItems: 'center',
+//     flex: 1,
+//     // borderRadius: 20,
+//     backgroundColor: 'white',
+//     // borderTopEndRadius: 60,
+//     // borderTopStartRadius: 60,
+//     borderTopLeftRadius: 10,
+//     borderTopRightRadius: 10,
+//     elevation: 8,
+//     borderBottomWidth: 0,
+//   },
+//   TabContainerBehind: {
+//     alignItems: 'center',
+//     flex: 1,
+//     // borderRadius: 20,
+//     backgroundColor: 'white',
+//     // borderTopEndRadius: 60,
+//     // borderTopStartRadius: 60,
+//     borderTopLeftRadius: 10,
+//     borderTopRightRadius: 10,
+//     elevation: 2,
+//   },
+// });
