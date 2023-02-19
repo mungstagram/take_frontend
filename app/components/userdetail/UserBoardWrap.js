0e83ede01a6265836b78300816f6b1c8ba9214ec
SUHONG\topic <topic828@Naver.com>
2023-02-19 16:39:16 +0900

    :sparkles: UserDetail 게시판 텝 컴퍼넌트 만들기
    사진 눌렀을 때, 동영상 눌렀을 때 디자인 바뀌는 것 구현

--- app/components/userdetail/UserBoardWrap.js
+++ app/components/userdetail/UserBoardWrap.js
@@ -1,113 +1,138 @@
-import {relative} from 'path';
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Pressable} from 'react-native';
+
 import {Colors} from '../../constants/colors';
 
 const UserBoardWrap = () => {
+  const [mainSelector, setMainSelector] = useState(true);
+  const showImageBoard = () => {
+    setMainSelector(true);
+  };
+
+  const showVideoBoard = () => {
+    setMainSelector(false);
+  };
+
+  //ContentGetter에다가 값주면 됨
   return (
     <View style={styles.Wrapper}>
-      <View style={styles.TabWrapper}></View>
-      <View style={styles.ContentWrapper}></View>
+      <View style={styles.TabWrapper}>
+        <View style={dynamicStyles(mainSelector).TabLeftBox}>
+          <Pressable onPress={showImageBoard}>
+            <Text style={dynamicStyles(mainSelector).TabTextLeft}> 사진</Text>
+          </Pressable>
+        </View>
+        <View style={dynamicStyles(mainSelector).TabRightBox}>
+          <Pressable onPress={showVideoBoard}>
+            <Text style={dynamicStyles(mainSelector).TabTextRight}>동영상</Text>
+          </Pressable>
+        </View>
+      </View>
+      <View style={styles.ContentWrapper}>
+        <View style={dynamicStyles(mainSelector).ContentLeftLayout}></View>
+        <View style={dynamicStyles(mainSelector).ContentRightLayout}></View>
+      </View>
+      <View style={styles.ContentGetterLayout}>
+        <Text> 하이</Text>
+      </View>
     </View>
   );
 };
 
 export default UserBoardWrap;
 
+const dynamicStyles = value =>
+  StyleSheet.create({
+    TabLeftBox: {
+      width: '50%',
+      height: '99%',
+      position: 'absolute',
+      left: 0,
+      bottom: 0,
+      backgroundColor: 'white',
+      borderTopStartRadius: 10,
+      borderTopEndRadius: 10,
+      zIndex: value ? 2 : 1,
+      elevation: value ? 25 : 8,
+      justifyContent: 'center',
+    },
+
+    TabRightBox: {
+      width: '50%',
+      height: '99%',
+      position: 'absolute',
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'white',
+      borderTopStartRadius: 10,
+      borderTopEndRadius: 10,
+      zIndex: value ? 1 : 2,
+      elevation: value ? 8 : 25,
+      justifyContent: 'center',
+    },
+
+    ContentLeftLayout: {
+      position: 'absolute',
+      height: '100%',
+      left: 0,
+      width: '50%',
+      backgroundColor: 'white',
+      zIndex: value ? 3 : 1,
+      elevation: value ? 0 : 10,
+    },
+    ContentRightLayout: {
+      position: 'absolute',
+      borderTopRightRadius: 10,
+      height: '100%',
+      right: 0,
+      width: '50%',
+      backgroundColor: 'white',
+      zIndex: value ? 1 : 3,
+      elevation: value ? 10 : 0,
+    },
+
+    TabTextLeft: {
+      textAlign: 'center',
+      marginBottom: '5%',
+      color: value ? 'blue' : 'black',
+      fontWeight: value ? 'bold' : 'normal',
+    },
+    TabTextRight: {
+      textAlign: 'center',
+      marginBottom: '5%',
+      color: value ? 'black' : 'blue',
+      fontWeight: value ? 'normal' : 'bold',
+    },
+  });
+
 const styles = StyleSheet.create({
   Wrapper: {
     flex: 1,
-    backgroundColor: 'green', // 여백있는지 표시
+    backgroundColor: 'transparent', // 여백있는지 표시
   },
   TabWrapper: {
     height: '15%',
     width: '100%',
-
     position: 'absolute',
     top: 0,
-    backgroundColor: 'red',
+    backgroundColor: 'white',
   },
+
   ContentWrapper: {
     width: '100%',
-    height: '90%',
+    height: '89%',
     position: 'absolute',
     bottom: 0,
-    backgroundColor: 'gray',
+    // backgroundColor: 'red', // 임시
+    zIndex: 2,
+  },
+
+  ContentGetterLayout: {
+    position: 'absolute',
+    height: '85%',
+    width: '100%',
+    bottom: 0,
+    backgroundColor: 'white',
     zIndex: 4,
   },
 });
-
-// import React from 'react';
-// import {View, Text, StyleSheet} from 'react-native';
-// import {Colors} from '../../constants/colors';
-
-// const UserBoardWrap = () => {
-//   return (
-//     <View style={styles.Wrapper}>
-//       <View style={styles.Container2}></View>
-//       <View style={styles.Container1}>
-//         <View style={styles.TabContainer}>
-//           <Text> 가나다</Text>
-//         </View>
-//         <View style={styles.TabContainerBehind}>
-//           <Text> 가나다</Text>
-//         </View>
-//       </View>
-//     </View>
-//   );
-// };
-
-// export default UserBoardWrap;
-
-// const styles = StyleSheet.create({
-//   Wrapper: {
-//     flex: 1,
-//     backgroundColor: 'green', // 여백있는지 표시
-//     // marginTop: 10,
-//     flexDirection: 'column-reverse',
-//   },
-//   Wrapper2: {
-//     flex: 1,
-//     backgroundColor: 'blue',
-//   },
-//   TextStyle: {
-//     fontSize: 16,
-//   },
-//   Container1: {
-//     flex: 0.1,
-//     // : ,
-//     // width: 20,
-//     backgroundColor: 'red',
-//     flexDirection: 'row',
-//     // alignItems: 'center',
-//     alignItems: 'stretch',
-//   },
-//   Container2: {
-//     flex: 1.2,
-//     backgroundColor: 'white',
-//     elevation: 8,
-//   },
-//   TabContainer: {
-//     alignItems: 'center',
-//     flex: 1,
-//     // borderRadius: 20,
-//     backgroundColor: 'white',
-//     // borderTopEndRadius: 60,
-//     // borderTopStartRadius: 60,
-//     borderTopLeftRadius: 10,
-//     borderTopRightRadius: 10,
-//     elevation: 8,
-//     borderBottomWidth: 0,
-//   },
-//   TabContainerBehind: {
-//     alignItems: 'center',
-//     flex: 1,
-//     // borderRadius: 20,
-//     backgroundColor: 'white',
-//     // borderTopEndRadius: 60,
-//     // borderTopStartRadius: 60,
-//     borderTopLeftRadius: 10,
-//     borderTopRightRadius: 10,
-//     elevation: 2,
-//   },
-// });
