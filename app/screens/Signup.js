e11fcf9a3c7e99adeea10655d4b4e9828909abf2
SUHONG\topic <topic828@Naver.com>
2023-02-23 14:02:49 +0900

    :sparkles: 회원가입 기능 및 전반적인 기능 추가 및 업데이트
    회원가입 배치 및 디자인, 인풋 라벨 추가, 중복확인 버튼,store에서 중복확인 여부 전역관리, 회원가입버튼 비활성화 추가,회원가입 여부 마운트 및 언마운트따라 관리, 회원가입 성공시  페이지 이동, 페이지 이동시 회원가입 여부 초기화,  유효성  조건 실패시 인풋박스에 밑줄

--- app/screens/Signup.js
+++ app/screens/Signup.js
@@ -1,14 +1,24 @@
-import {View, Text, StyleSheet, Alert} from 'react-native';
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import {View, Text, StyleSheet, Alert, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
 
 import AuthInput from '../components/AuthInput';
 import AuthButton from '../components/common/AuthButton';
 import AuthNavigateButton from '../components/AuthNavigateButton';
-import {__postUsers} from '../redux/modules/loginSlice';
-
+import {
+  uncheckEmail,
+  uncheckNick,
+  __checkUser,
+  __postUsers,
+} from '../redux/modules/loginSlice';
+import {Colors} from '../constants/colors';
 const Signup = () => {
   const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  // 비밀번호 보여줄 지 말지 선택 버튼
+  const [secureSetter, setSecureSetter] = useState(true);
 
   //입력 초기값
   const initialState = {
@@ -55,6 +65,44 @@ const Signup = () => {
         ? setPassCheckInput('비밀번호가 불일치합니다')
         : setPassCheckInput('');
   };
+
+  //버튼 비활성화 변수 선언
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+
+  // 특정값들 업데이트시 버튼 비활성화 버튼 활성화
+  useEffect(() => {
+    if (
+      password.trim() !== '' &&
+      passwordCheck.trim() !== '' &&
+      !passCheckInput &&
+      !passInput
+    ) {
+      setSubmitDisabled(false);
+    }
+  }, [password, passwordCheck, email, nickname]);
+
+  //중복확인 여부 스테이트
+  const {isEmailChecked} = useSelector(state => state.login);
+  const {isNickNameChecked} = useSelector(state => state.login);
+  console.log(isEmailChecked, '모듈 스테이트 확인');
+  //아이디 및 닉네임 중복확인 기능 함수
+  const onCheckUsabilityHandler = (name, value) => {
+    if (
+      (name === 'email' && emailInput === '' && email.trim() !== '') ||
+      (name === 'nickname' && nicknameInput === '' && nickname.trim() !== '')
+    ) {
+      dispatch(__checkUser({[name]: value}));
+    }
+  };
+  // 이메일 및 닉네임 값 업데이트시 체크 state해제
+  useEffect(() => {
+    dispatch(uncheckEmail);
+  }, [email]);
+
+  useEffect(() => {
+    dispatch(uncheckNick);
+  }, [nickname]);
+  // 제출버튼
   const onSubmitUserHandler = e => {
     e.preventDefault();
     if (
@@ -64,6 +112,12 @@ const Signup = () => {
       nickname.trim() === ''
     ) {
       return Alert.alert('모든 내용을 작성해주세요');
+    }
+    if (!isEmailChecked) {
+      return Alert.alert('이메일 중복확인 해주세요');
+    }
+    if (!isNickNameChecked) {
+      return Alert.alert('닉네임 중복확인 해주세요');
     }
 
     dispatch(
@@ -75,6 +129,16 @@ const Signup = () => {
     );
   };
 
+  // 사인업 성공시 true됨
+
+  const {isSuccessedSignup} = useSelector(state => state.login);
+  // 회원가입 성공시 페이지 이동
+  useEffect(() => {
+    if (isSuccessedSignup) {
+      navigation.replace('AuthStartScreen');
+    }
+  }, [isSuccessedSignup]);
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.buttonPosionter}>
@@ -82,35 +146,69 @@ const Signup = () => {
       </View>
       <View style={styles.inputWrapper}>
         <View>
-          <AuthInput
-            placeholder="id"
-            helper={emailInput}
-            onUpdateValue={onChangeUserHandler.bind(this, 'email')}
-            value={email}
-            // keyboardType="email-address"
-          />
-          <AuthInput
-            placeholder="nickname"
-            helper={nicknameInput}
-            onUpdateValue={onChangeUserHandler.bind(this, 'nickname')}
-            value={nickname}
-          />
+          <View>
+            <AuthInput
+              placeholder="id"
+              checkUsability
+              helper={emailInput}
+              onUpdateValue={onChangeUserHandler.bind(this, 'email')}
+              value={email}
+              isInvalid={regEmail.test(email)}
+              // keyboardType="email-address"
+            />
+            <View style={styles.buttonResizer}>
+              <View style={dynamicStyles().buttonPositioner}>
+                <Pressable
+                  style={dynamicStyles().buttonPositioner}
+                  onPress={() => onCheckUsabilityHandler('email', email)}>
+                  <Text style={dynamicStyles().buttonText}>중복 확인</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+          <View>
+            <AuthInput
+              placeholder="nickname"
+              checkUsability
+              helper={nicknameInput}
+              onUpdateValue={onChangeUserHandler.bind(this, 'nickname')}
+              value={nickname}
+              isInvalid={regNickname.test(nickname)}
+            />
+            <View style={styles.buttonResizer}>
+              <View style={dynamicStyles().buttonPositioner}>
+                <Pressable
+                  style={dynamicStyles().buttonPositioner}
+                  onPress={() => onCheckUsabilityHandler('nickname', nickname)}>
+                  <Text style={dynamicStyles().buttonText}>중복 확인</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
           <AuthInput
             placeholder="password"
             helper={passInput}
             onUpdateValue={onChangeUserHandler.bind(this, 'password')}
             value={password}
-            secure
+            secure={secureSetter}
+            setSecureSetter={setSecureSetter}
+            isInvalid={regPassword.test(password)}
           />
           <AuthInput
             placeholder="password-check"
             helper={passCheckInput}
             onUpdateValue={onChangeUserHandler.bind(this, 'passwordCheck')}
             value={passwordCheck}
-            secure
+            secure={secureSetter}
+            setSecureSetter={setSecureSetter}
+            isInvalid={password === passwordCheck}
           />
         </View>
-        <AuthButton onPress={onSubmitUserHandler}>입력 완료</AuthButton>
+        <View style={styles.submitButtonPositioner}>
+          <AuthButton disabled={submitDisabled} onPress={onSubmitUserHandler}>
+            입력 완료
+          </AuthButton>
+        </View>
       </View>
     </View>
   );
@@ -136,8 +234,38 @@ const styles = StyleSheet.create({
   },
   inputWrapper: {
     width: '90%',
-    backgroundColor: 'white',
+    // backgroundColor: 'red',
     position: 'absolute',
-    top: '30%',
+    top: '25%',
+  },
+  buttonResizer: {
+    position: 'absolute',
+    right: 12,
+    height: '100%',
+    paddingBottom: 12,
+    justifyContent: 'center',
+  },
+  submitButtonPositioner: {
+    marginTop: 18,
   },
 });
+
+const dynamicStyles = value =>
+  StyleSheet.create({
+    buttonPositioner: {
+      height: '68%',
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 90,
+      borderRadius: 4,
+    },
+    buttonText: {
+      color: Colors.mainColorBright,
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+    buttonView: {
+      opacity: value === '' ? 1 : 0.5,
+    },
+  });
