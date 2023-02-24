ce6fed5bf5f9bcd9332773bae0a5d654457ae533
olive-jam <120158932+olive-jam@users.noreply.github.com>
2023-02-25 02:57:16 +0900

    :sparkles: VideoDetail 폼 생성 시작
    VideoDetail 폼 생성 시작

---
+++ app/screens/VideoDetail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+
+const VideoDetail = () => {
+  return (
+    <View>
+      <View>
+        <View></View>
+        <Text></Text>
+        <Text></Text>
+      </View>
+      <View>
+        <Text> 영상자리 </Text>
+      </View>
+      <View>
+        <Text>Title</Text>
+        <View>
+          <Text>종아요</Text>
+        </View>
+        <View>
+          <Text>좋아요카운터</Text>
+        </View>
+        <Text>Content</Text>
+        <Text>더보기</Text>
+      </View>
+    </View>
+  );
+};
+
+export default VideoDetail;
