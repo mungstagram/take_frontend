57b7a33ac0b6611209f6c5c1248256230866883f
olive-jam <120158932+olive-jam@users.noreply.github.com>
2023-02-26 01:38:57 +0900

    :sparkles: VideoBoard 프로필이미지 추가
    VideoBoard 프로필이미지 추가

--- app/screens/VideoDetail.js
+++ app/screens/VideoDetail.js
@@ -1,30 +1,98 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, SafeAreaView} from 'react-native';
 
-const VideoDetail = () => {
+import {Colors, BasicColors} from '../constants/colors';
+import Favorite from '../components/svg/Favorite';
+import NotFavorite from '../components/svg/NotFavorite';
+
+const VideoDetail = ({videoContent}) => {
+  //const videoUrl = videoContent.contentUrl[0];
+
   return (
-    <View>
-      <View>
-        <View></View>
-        <Text></Text>
-        <Text></Text>
-      </View>
-      <View>
-        <Text> 영상자리 </Text>
-      </View>
-      <View>
-        <Text>Title</Text>
-        <View>
-          <Text>종아요</Text>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.detailTop}>
+          <View style={styles.profileImg}></View>
+          <View>
+            <Text style={styles.nicknameText}>lulu</Text>
+            <Text style={styles.timeText}>000</Text>
+          </View>
         </View>
-        <View>
-          <Text>좋아요카운터</Text>
+        <View style={styles.videoScreen}>
+          <Text> 영상자리 </Text>
         </View>
-        <Text>Content</Text>
-        <Text>더보기</Text>
+        <View style={styles.detailBottom}>
+          <View style={styles.preContent}>
+            <Text>Title</Text>
+            <View style={styles.favoritBox}>
+              <NotFavorite />
+              <Text>000</Text>
+            </View>
+          </View>
+          <View style={styles.contentBox}>
+            <Text>Content</Text>
+            <Text>더보기</Text>
+          </View>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default VideoDetail;
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const videoCardWidth = windowWidth;
+const videoCardHeight = videoCardWidth * 0.8;
+
+const styles = StyleSheet.create({
+  container: {
+    width: videoCardWidth,
+    height: videoCardHeight,
+  },
+  detailTop: {
+    backgroundColor: BasicColors.whiteColor,
+    flexDirection: 'row',
+    padding: '2%',
+    borderTopRightRadius: 4,
+    borderTopLeftRadius: 4,
+  },
+  profileImg: {
+    width: 24,
+    height: 24,
+    backgroundColor: BasicColors.grayColor,
+    borderRadius: 100,
+    marginRight: 10,
+  },
+  nicknameText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  timeText: {
+    fontSize: 8,
+  },
+  videoScreen: {
+    width: videoCardWidth,
+    height: videoCardHeight * 0.703,
+    backgroundColor: BasicColors.blackColor,
+  },
+  detailBottom: {
+    backgroundColor: BasicColors.whiteColor,
+    height: videoCardHeight * 0.3,
+    padding: '2%',
+  },
+  preContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '60%',
+  },
+  favoritBox: {
+    alignItems: 'center',
+  },
+  contentBox: {
+    flexDirection: 'row',
+  },
+});
