b0a93418d3d2ff8a36bfa525a2c83cfa0c4cd811
SUHONG\topic <topic828@Naver.com>
2023-03-12 15:22:19 +0900

    :sparkles: mainactivity 키해시 얻는 코드
    mainactivity 키해시 얻는 코드

--- android/app/src/main/java/com/pupfluencer/MainActivity.java
+++ android/app/src/main/java/com/pupfluencer/MainActivity.java
@@ -6,18 +6,48 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 import org.devio.rn.splashscreen.SplashScreen;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends ReactActivity {
   @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
+        getHashKey();
     }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  private void getHashKey(){
+    PackageInfo packageInfo = null;
+    try {
+      packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+    if (packageInfo == null)
+      Log.e("KeyHash", "KeyHash:null");
+
+    for (Signature signature : packageInfo.signatures) {
+      try {
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        md.update(signature.toByteArray());
+        Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+      } catch (NoSuchAlgorithmException e) {
+        Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+      }
+    }
+  }
+  
   @Override
   protected String getMainComponentName() {
     return "PupFluencer";
